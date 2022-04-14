37dca6158ef0531934f4dab6da178efa4f18dd5e
Jon Hogan <jon.hogan83@gmail.com>
2022-04-14 14:55:20 -0500

    Challenge 2 and 3

---
+++ Challenge2/Challenge2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Bow before Zod!");
+Console.WriteLine("Maybe Mr. T hacked the system and made a Mohawk class!");
+Console.WriteLine("He didn't, but still.");
+Console.WriteLine("Chuck Norris doesn't do push-ups, he pushes the Earth down");
+Console.WriteLine("My name is Jon.");
--- P1_Hello_World/Program.cs
+++ P1_Hello_World/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// Of course there is a Hello World. What programming book would be complete without it??
 Console.WriteLine("Hello, World!");
